3c962b0d34f5c9271677d3f5289285fb982707c8
Victor Ly <ly.victor2000@gmail.com>
2026-04-13 10:23:18 +0200

    First commit

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ToiletFinder3000.Pages;
+
+public class IndexModel : PageModel
+{
+    public void OnGet()
+    {
+
+    }
+}
