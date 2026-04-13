5c3cfcca97cef361e6db27177a1c055093579547
Victor Ly <ly.victor2000@gmail.com>
2026-04-13 11:09:42 +0200

    Add toilets to repository and update Index page to display toilet nicknames

--- Repository/ToiletRepository.cs
+++ Repository/ToiletRepository.cs
@@ -13,9 +13,11 @@ namespace ToiletFinder3000.Repository
 
 			Address address1 = new Address("Lyngvej 21", "4600", "Køge");
 			Toilet toilet1 = new Toilet("1", "Skibidi", address1);
+			_toilets.Add(toilet1);
 
 			Address address2 = new Address("Maglegårdsvej 2", "4000", "Roskilde");
 			Toilet toilet2 = new Toilet("2", "Six-seven", address2);
+			_toilets.Add(toilet2);
 		}
 		public List<Toilet> GetAllToilets()
 		{
